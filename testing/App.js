cbf7755a200faf943974c14e51a7bc1fc7e1775a
saltysodapop <ana.obradovic2017@gmail.com>
2024-06-08 21:42:01 -0400

    first commit

---
+++ testing/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>brb im gonna go invade denmark for the bread</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
