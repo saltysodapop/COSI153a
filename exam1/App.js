8d2164e1a847169782e7dcafa1c7b806da836952
saltysodapop <ana.obradovic2017@gmail.com>
2024-06-10 13:12:04 -0400

    exam1 layout done

--- exam1/App.js
+++ exam1/App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TextInput } from 'react-native-web'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Calculate area of triangle by Herron's rule</Text>
+      <Text style={styles.bigText}>Calculate area of triangle by Herron's rule</Text>
+      <Text style={styles.text}>Write the code for this app which calculates</Text>
+      <Text style={styles.text}>area = Math.sqrt(s*(s-a)*(s-b)*(s-c))</Text>
+      <Text style={styles.text}>where s = (a+b+c)/2</Text>
+      <TextInput style={styles.input}></TextInput>
+      <TextInput style={styles.input}></TextInput>
+      <TextInput style={styles.input}></TextInput>
+      <Button title = 'calculate area'></Button>
+      <Text style={styles.text}>area is </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,12 +27,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderColor: 'blue',
-    borderWidth: 4,
-    marginVertical: 35,
-    marginHorizontal: 35,
+    borderWidth: 3,
+    marginVertical: 30,
+    marginHorizontal: 30,
   },
   text: {
+    fontSize: 14,
+  },
+  bigText: {
     fontWeight: 'bold',
-    fontSize: 30,
-  }
+    fontSize: 20,
+  },
+  input: {
+    backgroundColor: 'yellow',
+  },
 });
